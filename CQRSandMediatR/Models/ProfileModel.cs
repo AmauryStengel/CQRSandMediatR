e9e1b5e60bf0ace7d5f25f0fb7826c78d5a94a95
Amaury Stengel Vieira <amaurystengel@hotmail.com>
2023-02-27 09:06:00 -0300

    create class profile.

--- CQRSandMediatR/Models/ProfileModel.cs
+++ CQRSandMediatR/Models/ProfileModel.cs
@@ -2,5 +2,15 @@
 {
     public class ProfileModel
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CPF { get; set; }
+        public string BirthDate { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public string Address { get; set; }
+        public string CellPhone { get; set; }
+        public string CivilStatus { get; set; }
     }
 }
