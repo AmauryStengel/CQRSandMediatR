f9be93d5db1d1633c7da4c761a5f0d5007d9192a
Amaury Stengel Vieira <amaurystengel@hotmail.com>
2023-03-01 11:08:25 -0300

    - Configurated way to connect to database in DbProfileData
    - Created variable DefaultConnection in appsettings.json

--- CQRSandMediatR/Data/DbProfileData.cs
+++ CQRSandMediatR/Data/DbProfileData.cs
@@ -1,6 +1,20 @@
-﻿namespace CQRSandMediatR.Data
+﻿using CQRSandMediatR.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSandMediatR.Data
 {
-    public class DbProfileData
+    public class DbProfileData : DbContext
     {
+        protected readonly IConfiguration configuration;
+
+        public DbProfileData(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<ProfileModel> Profiles { get; set; }
     }
 }
