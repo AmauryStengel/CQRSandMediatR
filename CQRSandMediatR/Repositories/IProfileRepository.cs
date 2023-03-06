2908d9cbb6de0720f761ec85f073ca8acbb3aa81
Amaury Stengel Vieira <amaurystengel@hotmail.com>
2023-03-06 09:00:03 -0300

    Implementação de interface da camada de repositório

--- CQRSandMediatR/Repositories/IProfileRepository.cs
+++ CQRSandMediatR/Repositories/IProfileRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace CQRSandMediatR.Repositories
+﻿using CQRSandMediatR.Models;
+
+namespace CQRSandMediatR.Repositories
 {
     public interface IProfileRepository
     {
+        public Task<List<ProfileModel>> GetProfiles();
+        public Task<ProfileModel> GetProfileById(int id);
+        public Task<ProfileModel> AddProfile(ProfileModel profile);
+        public Task<ProfileModel> UpdateProfile(ProfileModel profile);
+        public Task<ProfileModel> DeleteProfile(int id);
     }
 }
--- CQRSandMediatR/Repositories/ProfileRepository.cs
+++ CQRSandMediatR/Repositories/ProfileRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace CQRSandMediatR.Repositories
+﻿using CQRSandMediatR.Models;
+
+namespace CQRSandMediatR.Repositories
 {
-    public class ProfileRepository
+    public class ProfileRepository : IProfileRepository
     {
+        public Task<ProfileModel> AddProfile(ProfileModel profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProfileModel> DeleteProfile(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProfileModel> GetProfileById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProfileModel>> GetProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProfileModel> UpdateProfile(ProfileModel profile)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
