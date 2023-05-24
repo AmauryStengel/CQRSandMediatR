7d979b532d57843f7179847c6a5f703301bb01e6
Amaury Stengel Vieira <amaurystengel@hotmail.com>
2023-05-24 09:11:02 -0300

    Add Role, Get Token

--- CQRSandMediatR/Controllers/AuthenticateController.cs
+++ CQRSandMediatR/Controllers/AuthenticateController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CQRSandMediatR.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace CQRSandMediatR.Controllers
 {
@@ -19,6 +23,32 @@ namespace CQRSandMediatR.Controllers
             _configuration = configuration;
             _userManager = userManager;
             _roleManager = roleManager;
+        }
+
+        private TokenModel GetToken(List<Claim> authClaims)
+        {
+            var authSignInKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSignInKey, SecurityAlgorithms.HmacSha256)
+                );
+            return new()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                ValidTo = token.ValidTo //
+            };
+        }
+
+        private async Task AddToRoleAsync(IdentityUser user, string role)
+        {
+            if (!await _roleManager.RoleExistsAsync(role))
+            {
+                await _roleManager.CreateAsync(new(role));
+            }
+            await _userManager.AddToRoleAsync(user, role);
         }
     }
 }
